0194010662ae3c8752c22c1ce6ad46588b38e9c3
천광규 <chonkk@castis.com>
2021-08-06 00:40:49 +0900

    test

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class DemoApplication 
+implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		log.info("test");
+	}
+
+}
